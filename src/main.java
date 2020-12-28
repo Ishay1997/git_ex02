2ac3cc12de57d469d2382ea6630d65020bbab720
ישי לוי <ishaylevy8@gmail.com>
2020-12-28 15:10:36 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class main {
+    public static void main(String[] args) {
+        try{
+            Desktop.getDesktop().browse(new URI("https://ynet.co.il"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        catch(URISyntaxException e){
+            e.printStackTrace();
+        }
+    }
+}
